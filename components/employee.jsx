541ccb212e15bb98416192519b8948e0bfa611ef
Pavlov Oleh <opavlov@conceptsoft.io>
2021-07-30 04:34:55 +0900

    added employee dummy ui

--- components/employee.jsx
+++ components/employee.jsx
@@ -1,14 +1,30 @@
 export default function EmployeeComponent() {
     return(
-        <div className="flex justify-between p-4">
-            <div className='w-24 h-24 overflow-hidden rounded-full'>
-                <img 
-                    src='https://image.pngaaa.com/879/2874879-middle.png'
-                    className=''
-                />
+        <div className=" relative p-4 text-xs leading-6 text-white">
+            <div className="h-24 flex">
+                <div className='w-24 h-24 mr-2 overflow-hidden rounded-full'>
+                    <img src='https://image.pngaaa.com/879/2874879-middle.png' />
+                </div>
+                <div className='flex-1 grid grid-cols-6 grid-rows-3 gap-2'>
+                    <div className='row-span-1 col-span-3 px-2 bg-gray-600 rounded-sm'>
+                        Name: Ophelia
+                    </div>
+                    <div className='row-span-2 col-span-3 px-2 bg-gray-600 rounded-sm'>
+                        Summary: Best in her field!
+                    </div>
+                    <div className='row-span-1 col-span-3 px-2 bg-gray-600 rounded-sm'>
+                        Title: Full stack developer
+                    </div>
+                    <div className='row-span-1 col-span-2 col-start-5'>
+                        <button className='w-full rounded-sm bg-gradient-to-r from-red-600 to-yellow-500'>
+                            Review
+                        </button>
+                    </div>
+                </div>
+            </div> 
+            <div className='absolute bottom-0 left-0 right-0 h-1 rounded-b-sm bg-gradient-to-r from-red-600 to-green-600'>
+
             </div>
-            
-            <span className='flex-1'></span>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import EmployeeComponent from '../components/employee';
 
 export default function HomePage() {
   return(
-    <div className='container mx-auto'>
+    <div className='container mx-auto mt-5'>
       <div className='grid grid-cols-12 gap-10 py-15 px-10 lg:px-25'>
-        <div className='col-span-12 lg:col-span-3 bg-gray-200'>Sidebar</div>
-        <div className='col-span-12 lg:col-span-9 bg-blue-200'>
+        <div className='col-span-12 lg:col-span-3 bg-gray-800 rounded-sm'>Sidebar</div>
+        <div className='col-span-12 lg:col-span-9 bg-gray-800 rounded-sm'>
           <EmployeeComponent />
         </div>
       </div>
