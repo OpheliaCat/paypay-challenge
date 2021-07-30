cde31255166f15f41de9667f7a8d6376a0da7500
Pavlov Oleh <opavlov@conceptsoft.io>
2021-07-30 14:18:47 +0900

    added dummy data & rendered employees in loop

--- components/employee.jsx
+++ components/employee.jsx
@@ -1,19 +1,19 @@
-export default function EmployeeComponent() {
+export default function EmployeeComponent({ name, pictureURL, jobTitle, summary, performance }) {
     return(
-        <div className=" relative p-4 text-xs leading-6 text-white">
+        <div className=" relative mt-4 p-4 bg-gray-700 text-xs leading-6 text-white">
             <div className="h-24 flex">
                 <div className='w-24 h-24 mr-2 overflow-hidden rounded-full'>
-                    <img src='https://image.pngaaa.com/879/2874879-middle.png' />
+                    <img src={pictureURL || 'https://image.pngaaa.com/879/2874879-middle.png'} />
                 </div>
                 <div className='flex-1 grid grid-cols-6 grid-rows-3 gap-2'>
                     <div className='row-span-1 col-span-3 px-2 bg-gray-600 rounded-sm'>
-                        Name: Ophelia
+                        <span className='text-red-300'>Name: </span> { name }
                     </div>
                     <div className='row-span-2 col-span-3 px-2 bg-gray-600 rounded-sm'>
-                        Summary: Best in her field!
+                        <span className='text-red-300'>Summary: </span> { summary }
                     </div>
                     <div className='row-span-1 col-span-3 px-2 bg-gray-600 rounded-sm'>
-                        Title: Full stack developer
+                        <span className='text-red-300'>JobTitle: </span> { jobTitle}
                     </div>
                     <div className='row-span-1 col-span-2 col-start-5'>
                         <button className='w-full rounded-sm bg-gradient-to-r from-red-600 to-yellow-500'>
@@ -22,8 +22,10 @@ export default function EmployeeComponent() {
                     </div>
                 </div>
             </div> 
-            <div className='absolute bottom-0 left-0 right-0 h-1 rounded-b-sm bg-gradient-to-r from-red-600 to-green-600'>
-
+            <div 
+                className='absolute bottom-0 left-0 right-0 h-1 rounded-b-sm bg-gradient-to-r from-red-600 to-green-600'
+                style={{ width: `${performance}%` }}    
+            >
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
 import EmployeeComponent from '../components/employee';
 
+const dummyEmployees = [
+  {
+    name: 'Ophelia',
+    pictureURL: 'https://image.pngaaa.com/879/2874879-middle.png',
+    jobTitle: 'Senior Full Stack developer', 
+    summary: 'best in her field', 
+    performance: '70'
+  },
+  {
+    name: 'Mayushi',
+    pictureURL: 'https://www.pngkey.com/png/full/145-1450218_26-nier-sucks-imagenes-de-lolis-kawaii.png',
+    jobTitle: 'Data scientist', 
+    summary: 'has too much of compensation', 
+    performance: '40'
+  }
+]
+
 export default function HomePage() {
   return(
     <div className='container mx-auto mt-5'>
       <div className='grid grid-cols-12 gap-10 py-15 px-10 lg:px-25'>
         <div className='col-span-12 lg:col-span-3 bg-gray-800 rounded-sm'>Sidebar</div>
-        <div className='col-span-12 lg:col-span-9 bg-gray-800 rounded-sm'>
-          <EmployeeComponent />
+        <div className='col-span-12 lg:col-span-9 p-2 bg-gray-800 rounded-sm'>
+          { 
+            dummyEmployees.map((data, i) => <EmployeeComponent {...data} key={i} />)
+          }
         </div>
       </div>
     </div>
